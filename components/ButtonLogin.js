8033b2adbdc1b4da303e02d722e887a02eeb3be8
Jennifer Bland <ratracegrad@gmail.com>
2024-12-11 19:21:46 -0500

    updated login button

--- components/ButtonLogin.js
+++ components/ButtonLogin.js
@@ -12,7 +12,7 @@ export default function ButtonLogin({ isLoggedIn }) {
 
   return (
     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      <Link href="/api/auth/login">Login</Link>
+      <Link href="/api/auth/logIn">Login</Link>
     </button>
   );
 }
